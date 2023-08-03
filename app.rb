1d1180ccc19f80b47954bef9805b68deca9123b8
Алексей <alyoshinaa@gmail.com>
2023-08-03 20:30:21 +0300

    make it work

---
+++ app.rb
@@ -0,0 +1,20 @@
+
+class App
+  def call(env)
+    [status, headers, body]
+  end
+
+  private
+
+  def status
+    200
+  end
+
+  def headers
+    { 'Content-type' => 'text/plain' }
+  end
+
+  def body
+    ['Welcome']
+  end
+end
